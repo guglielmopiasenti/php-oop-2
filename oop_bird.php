5619dcb96fbde2cab10737d2912c3c80d8fe9777
guglielmopiasenti <piasentiguglielmo@gmail.com>
2023-07-24 16:49:23 +0200

    oop_bird

---
+++ oop_bird.php
@@ -0,0 +1,11 @@
+<?php
+
+require_once 'oop_products.php';
+
+$bird = new Category("Bird");
+
+$birdProducts = array(
+    new Product("Voliera Wilma in Legno", "https://arcaplanet.vtexassets.com/arquivos/ids/258384/voliera-wilma1.jpg", $bird),
+);
+
+?>
